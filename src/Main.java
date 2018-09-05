5b1cb181f22815714c4ac6240ad003f1b8829586
Thomas Rognes <thomas_rog@hotmail.com>
2018-09-05 15:58:40 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,7 @@ public class Main {
         System.out.println("dette er en test for git");
         System.out.println("MAgnuS");
         System.out.println("TORD");
+        System.out.println("Test");
         connection.close();
 
     }
